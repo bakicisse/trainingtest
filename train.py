8b88ade4fff0113ca76997594ad855efc1c29b37
Baki CISSE <cissebaki@gmail.com>
2025-02-28 10:46:53 -0500

    message de fin

--- train.py
+++ train.py
@@ -1,1 +1,13 @@
 print("Hello, world!")
+for i in range(10):
+    print(i)
+print("Goodbye, world!")
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+for row in matrix:
+    for element in row:
+        print(element)
+    while True:
+        break   # This is a comment
+
+print("Done!")
